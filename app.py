a23633509b0c1aae492ad482c801ef8bd7d15fdd
dkaly <dimitrikaly@gmail.com>
2026-05-25 15:33:12 -0400

    Added ability to ask follow up questions and converse

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, session
 from flask_cors import CORS
 from urllib.parse import urlparse, parse_qs
 
 
 app = Flask(__name__)
 CORS(app)
+app.secret_key = "your_secret_key"
 
 #load api key
 load_dotenv()
@@ -17,12 +18,13 @@ client = OpenAI(api_key = os.getenv("OPEN_API_KEY"))
 
 @app.route("/")
 def home():
-    return "YouTube Summarizer AI backend is running"
+    return render_template("index.html")
 
 @app.route("/summarize", methods=["POST"])
 def summarize():
     data = request.json
     url = data["url"]
+
 
     #video id
     ###video_id = url.split("v=")[1].split("&")[0] #www.youtube.com/watch?v=  < UF8uR6Z6KLc >  &t=10s
@@ -40,6 +42,9 @@ def summarize():
     ytt_api = YouTubeTranscriptApi()
     transcript = ytt_api.fetch(video_id)
     text = " ".join([snippet.text for snippet in transcript])
+
+    session["transcript"] = text
+    session["chat_history"] = []
 
     #give to ai
     response = client.chat.completions.create(
@@ -73,6 +78,62 @@ def summarize():
     return jsonify({
         "summary": summary
     })
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    data = request.json
+    question = data["question"]
+
+    transcript = session.get("transcript", "")
+    history = session.get("chat_history", [])
+
+    if not transcript:
+        return jsonify({
+            "error": "No transcript found. Please summarize a video first."
+            }), 400
+
+    messages = [ 
+        {
+            "role": "system",
+            "content": f"""
+            You are answering follow-up questions about a YouTube video.
+
+            Only use information from transcript.
+
+            Transcript:
+            {transcript}
+            """
+        }
+    ]
+
+    messages.extend(history)
+    messages.append({
+        "role": "user",
+        "content": question
+    })
+
+    # Call the OpenAI API
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages = messages
+    )
+    # Append the AI response to the chat history
+    answer = response.choices[0].message.content
+
+    history.append({
+        "role": "user", 
+        "content": question
+        })
+    history.append({
+        "role": "assistant", 
+        "content": answer
+        })
+    
+    session["chat_history"] = history
+
+    return jsonify({
+        "answer": answer
+        })
 
 if __name__ == "__main__":  
     app.run(debug = True)
